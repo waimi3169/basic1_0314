65cd1e2430e43bacf79ea0caa996c38ca0f84c68
깃waimi3169 <waimi3169@naver.com>
2023-03-17 11:42:16 +0900

    서비스 구현

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -3,18 +3,22 @@ package com.ll.basic1.boundedContext.article.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Builder
 public class Article {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    private LocalDateTime createDate; // 데이터 생성 날짜
+    private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
 }
